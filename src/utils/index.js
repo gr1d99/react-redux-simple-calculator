3272b6bd5d6040c43c52f08f164fda4a914e1ed2
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-19 11:17:35 +0300

    Add more operation types

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,8 @@
 import * as opTypes from './operationTypes';
 
 export const operations = [
-    opTypes.ADD_OP
+    opTypes.ADD_OP,
+    opTypes.SUB_OP,
+    opTypes.MULTIPLY_OP,
+    opTypes.DIVIDE_OP
 ]; // default mathematical operations
--- src/utils/operationTypes.js
+++ src/utils/operationTypes.js
@@ -1,1 +1,4 @@
 export const ADD_OP = 'Add';
+export const SUB_OP = 'Subtract';
+export const MULTIPLY_OP = 'Multiply';
+export const DIVIDE_OP = 'Divide';
