c3eaf08f21b91ba382ef4ef8b3c7134ad7f3a1fe
Gideon Kimutai <gideonkimutai9@gmail.com>
2018-02-20 09:40:04 +0300

    Add test config

---
+++ src/components/testSetup.js
@@ -0,0 +1,7 @@
+/* necessary configurations for enzyme */
+import React from 'react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+
+Enzyme.configure({adapter: new Adapter()});
